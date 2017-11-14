f3b4bb46f14e84f0905386092b347191e0065e62
John Sibly <john@sibly.co.uk>
2017-11-14 13:44:28 +0000

    Branch with Nalin's test

--- CSharp/PasswordReset.Tests/ComplexityValidatorTests.cs
+++ CSharp/PasswordReset.Tests/ComplexityValidatorTests.cs
@@ -22,5 +22,34 @@ namespace PasswordReset.Tests
 
             Assert.True(validator.Validate("ThisIsMyPassword", "ThisIsMyPassword"));
         }
+
+        [Test()]
+        public void Vaildate_ShouldBeGreaterThan8CharactersInLength()
+        {
+            PasswordComplexityValidator validator = new PasswordComplexityValidator();
+            Assert.False(validator.Validate("Passwor", "Passwor"));
+        }
+
+        [Test()]
+        public void Vaildate_MustNotBeGreaterThan20CharactersInLength()
+        {
+            PasswordComplexityValidator validator = new PasswordComplexityValidator();
+            Assert.False(validator.Validate("Password10Password10X", "Password10Password10X"));
+        }
+
+        [Test()]
+        public void Vaildate_MustContainALowerCaseCharacters()
+        {
+            PasswordComplexityValidator validator = new PasswordComplexityValidator();
+            Assert.False(validator.Validate("PASSWORD", "PASSWORD"));
+        }
+
+        [Test()]
+        public void Vaildate_MustContainAnUpperCaseCharacter()
+        {
+            PasswordComplexityValidator validator = new PasswordComplexityValidator();
+            Assert.False(validator.Validate("password", "password"));
+        }
+
     }
 }
--- CSharp/PasswordReset/PasswordComplexityValidator.cs
+++ CSharp/PasswordReset/PasswordComplexityValidator.cs
@@ -9,7 +9,46 @@ namespace PasswordReset
 
         public bool Validate(String password1, String password2)
         {
-            return password1 == password2;
+            if ( password1 == password2 &&
+                password1.Length >= 8 &&
+                password1.Length <= 20 && password2.Length <= 20 &&
+                containsLowerCase(password1) && containsLowerCase(password2) &&
+                containsUpperCase(password1) && containsUpperCase(password2)
+               )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                   
+        }
+
+        bool containsLowerCase(String password)
+        {
+            foreach (char c in password)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        bool containsUpperCase(String password)
+        {
+            foreach (char c in password)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
